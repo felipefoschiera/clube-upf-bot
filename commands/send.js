0df6afb072d0df7eb2ea1c369dcb3384d61f8456
felipefoschiera <felipefoschiera@gmail.com>
2022-04-17 14:11:26 -0300

    ✨ Add command to send messages to a channel

---
+++ commands/send.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('send')
+		.setDescription('Send a message on a chat!')
+		.addChannelOption(option =>
+			option.setName('channel')
+				.setDescription('The channel to send the message to.')
+				.setRequired(true),
+		)
+		.addStringOption(option =>
+			option.setName('text')
+				.setDescription('The text message content')
+				.setRequired(true),
+		),
+	async execute(interaction) {
+		const channel = interaction.options.getChannel('channel');
+		const text = interaction.options.getString('text');
+		channel.send(text);
+		await interaction.reply({ content: 'Message sent!', ephemeral: true });
+	},
+};
