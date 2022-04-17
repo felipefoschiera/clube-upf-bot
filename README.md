001828207df485a115058873883278fceff7d298
felipefoschiera <felipefoschiera@gmail.com>
2022-04-17 16:24:31 -0300

    🔧 Use environment variables for permissions JSON

--- README.md
+++ README.md
@@ -14,16 +14,4 @@ CLIENT_ID="<BOT_CLIENT_ID>"
 GUILD_ID="<SERVER_ID>"
 ```
 
-### `permissions.json`
-This file contains permissions that apply to all guild commands. Later, this can be command-specific. Expected format: 
-```json
-{
-    "permissions": [
-        {
-            "id": "<ROLE_ID>",
-            "type": "ROLE",
-            "permission": true
-        }
-    ]
-}
 ```
--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,9 @@
-const { permissions } = require('../permissions.json');
-
 module.exports = {
 	name: 'ready',
 	once: true,
 	async execute(client) {
 		const guild = client.guilds.cache.get(process.env.GUILD_ID);
+		const permissions = JSON.parse(process.env.PERMISSIONS);
 		const commands = await guild.commands.fetch();
 		await commands.forEach(slashCommand => {
 			console.log(`Definindo permissões para o comando "${slashCommand.name}" (${slashCommand.id})`);
