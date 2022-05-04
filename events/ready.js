99d7e381faf4c5255fcc79bf22b30b47f000bcf4
felipefoschiera <felipefoschiera@gmail.com>
2022-05-04 13:29:12 -0300

    🔥 Remove set permissions on bot ready. Now managed through Server Settings

--- events/ready.js
+++ events/ready.js
@@ -4,17 +4,6 @@ export const name = 'ready';
 export const once = true;
 
 export const execute = async (client) => {
-	const guild = client.guilds.cache.get(process.env.GUILD_ID);
-	const permissions = JSON.parse(process.env.PERMISSIONS);
-	const commands = await guild.commands.fetch();
-	await commands.forEach(slashCommand => {
-		console.log(`Definindo permissões para o comando "${slashCommand.name}" (${slashCommand.id})`);
-		guild.commands.permissions.add({
-			command: slashCommand.id,
-			permissions,
-		});
-	});
-
 	await setCodeforcesCronJob(client);
 	console.log('Bot está pronto!');
 };
